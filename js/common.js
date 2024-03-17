4ef31b358cb7bd3afa35c8d31ca9efc556bb04e1
BarybinAlexander <82830023+BarybinAlexander@users.noreply.github.com>
2024-03-17 10:27:25 +0300

    add delivery and adaptive

--- js/common.js
+++ js/common.js
@@ -1,1 +1,26 @@
-// $('body').hide()
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 24,
+        }
+    }
+});
